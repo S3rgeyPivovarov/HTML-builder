a1e640ac939671d6a760be618b1d66e4d1ab1696
S3rgeyPivovarov <111754932+S3rgeyPivovarov@users.noreply.github.com>
2023-05-08 21:22:21 +0300

    feat: add solve task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const inputLine = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Привет) Ожидание ввода");
+
+inputLine.on("line", (input) => {
+  if (input === "exit") {
+    console.log("Удачи)");
+    inputLine.close();
+  } else {
+    fs.appendFile("textTask2.txt", input + "\n", (err) => {
+      if (err) throw err;
+      console.log("Текст добавлен text.txt");
+    });
+  }
+});
